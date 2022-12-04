260ed7c0356ede91e2a04d7b57c5d083966c29db
Zinnybliss <chiwueze.ezinne@gmail.com>
2022-12-04 23:39:48 +0100

    first commit

---
+++ src/components/HeaderApp.vue
@@ -0,0 +1,31 @@
+<template>
+  <header>
+    <h1>{{ title }}</h1>
+    <ButtonApp
+      @btn-click="$emit('toggle-add-task')"
+      :text="showAddTask ? 'Close' : 'Add Task'"
+      :color="showAddTask ? 'red' : 'green'"
+    />
+  </header>
+</template>
+<script>
+import ButtonApp from "./ButtonApp.vue";
+export default {
+  components: {
+    ButtonApp,
+  },
+  name: "HeaderApp",
+  props: {
+    title: String,
+    showAddTask: Boolean,
+  },
+};
+</script>
+<style scoped>
+header {
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  margin-bottom: 20px;
+}
+</style>
---
+++ src/components/TasksApp.vue
@@ -0,0 +1,27 @@
+<template>
+  <div>
+    <div v-for="task in tasks" :key="task.id">
+      <TaskApp
+        @toggle-reminder="$emit('toggle-reminder', task.id)"
+        @delete-task="$emit('delete-task', task.id)"
+        :task="task"
+      />
+    </div>
+  </div>
+</template>
+
+<script>
+import TaskApp from "./TaskApp.vue";
+export default {
+  name: "TasksApp",
+  props: {
+    tasks: Array,
+  },
+  components: {
+    TaskApp,
+  },
+  emits: ["delete-task", "toggle-reminder"],
+};
+</script>
+
+<style></style>
